a0d4550ab4bb79f389ba4b17be567c7a064d5619
Ram K <82735640+anogaaram@users.noreply.github.com>
2022-11-08 01:50:42 +0000

    first

---
+++ example/pencill_data_example.dart
@@ -0,0 +1,6 @@
+import 'package:pencill_data/pencill_data.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/pencill_data.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library pencill_data;
+
+export 'src/pencill_data_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
